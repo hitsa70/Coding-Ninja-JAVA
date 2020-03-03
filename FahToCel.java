d8ef8196ded047f20ffe99b1b0f412c32d2cd24c
HITESH CHOUDHARY <hcjat70@gmail.com>
2020-03-03 19:49:47 +0530

    Update FahToCel.java

--- FahToCel.java
+++ FahToCel.java
@@ -6,27 +6,27 @@ public class FahToCel {
 //		Scanner s = new Scanner(System.in);
 //		int f = s.nextInt();
 //		//int cel = (5/9)*(f - 32);
-//		//int cel = (5 * (f - 32))/9;
+//		//int cel = (5 * (f - 32))/9;        Gives correct output
 //		
 //		int cel = (int)((5.0/9)*(f - 32));
 //		System.out.println(cel);
 		
-		System.out.println(9/5);
-		System.out.println(9.0/5);
+		System.out.println(9/5);  		// OUTPUT 1
+		System.out.println(9.0/5);              // OUTPUT 1.8
 		
-		System.out.println(9  + 5);
-		System.out.println(9  - 5);
-		System.out.println(9  / 5);
-		System.out.println(9  * 5);
+		System.out.println(9  + 5);		// OUTPUT 14
+		System.out.println(9  - 5);		// OUTPUT 4
+		System.out.println(9  / 5);		// OUTPUT  1
+		System.out.println(9  * 5);		// OUTPUT 45
 		
-		System.out.println('a'/3);
+		System.out.println('a'/3);		// OUTPUT 32
 		
-		System.out.println(9 % 5);
+		System.out.println(9 % 5);		// OUTPUT 4
 		char c = 'a';
 		char i = (char)(c + 3);
-		System.out.println(i);
+		System.out.println(i);			// OUTPUT  d
 		
-		System.out.println('a' + 'b');
+		System.out.println('a' + 'b');		// OUTPUT  195
 		
 		
 	}
