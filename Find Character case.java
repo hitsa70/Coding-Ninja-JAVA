fc59af4f96c4f2532f427cb15a89bf946d239bb5
HITESH CHOUDHARY <hcjat70@gmail.com>
2020-03-04 00:43:13 +0530

    Update Find Character case.java

--- Find Character case.java
+++ Find Character case.java
@@ -1,3 +1,30 @@
+//Find Character Case
+//Send Feedback
+//Write a program to determine whether the entered character is in uppercase or lowercase, or is an invalid character.
+//Print
+// 1 for uppercase
+// 0 for lowercase 
+//-1 for any other character (special characters or others)
+//Input format :
+//Single Character
+//Output format :
+//1 or 0 or -1
+//Sample Input 1 :
+//v
+//Sample Output 1 :
+//0
+//Sample Input 2 :
+//V
+//Sample Output 2 :
+//1
+//Sample Input 3 :
+//#
+//Sample Output 3 :
+//-1
+
+
+
+
 import java.util.Scanner;
 public class Main {
 	
